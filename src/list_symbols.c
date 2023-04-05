d8b9859485791b286a09376e1e8b7c2027a2c3d5
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-04-05 17:36:32 +0200

    feat: handle 32 bit headers

--- src/list_symbols.c
+++ src/list_symbols.c
@@ -21,7 +21,25 @@ static int			list(t_list **dest, t_nm_file *file)
 	return (1);
 }
 
-#define NM_UNDEFINED_TYPES "Uuvw"
+#define NM_UNDEFINED_TYPES	"Uuvw"
+#define NM_VALUE_PADDING_32	8
+#define NM_VALUE_PADDING_64	16
+
+static void			print(t_list *symbols, t_nm_file *file)
+{
+	for (t_list *ptr = symbols; ptr; ptr = ptr->next)
+	{
+		t_nm_symbol	*symbol = ptr->content;
+		if (ft_strchr(NM_UNDEFINED_TYPES, symbol->type))
+			ft_printf("%*c %c %s\n", file->class == ELFCLASS32
+				? NM_VALUE_PADDING_32 : NM_VALUE_PADDING_64,
+				' ', symbol->type, symbol->name);
+		else
+			ft_printf("%0*llx %c %s\n", file->class == ELFCLASS32
+				? NM_VALUE_PADDING_32 : NM_VALUE_PADDING_64,
+				symbol->value, symbol->type, symbol->name);
+	}
+}
 
 int					list_symbols(t_nm_file *file, t_nm_config *cfg)
 {
@@ -36,14 +54,7 @@ int					list_symbols(t_nm_file *file, t_nm_config *cfg)
 	}
 	// SORT (with strcoll)
 	// PRINT
-	for (t_list *ptr = symbols; ptr; ptr = ptr->next)
-	{
-		t_nm_symbol	*symbol = ptr->content;
-		if (ft_strchr(NM_UNDEFINED_TYPES, symbol->type))
-			ft_printf("%16c %c %s\n", ' ', symbol->type, symbol->name);
-		else
-			ft_printf("%016llx %c %s\n", symbol->value, symbol->type, symbol->name);
-	}
+	print(symbols, file);
 	// CLEAN
 	ft_lstdel(&symbols, delete_symbol);
 	return (0);
--- src/symbol.c
+++ src/symbol.c
@@ -19,6 +19,78 @@ void	delete_symbol(void *symbol, size_t size)
 	ft_memdel(&symbol);
 }
 
+char	symbol_type32(Elf32_Sym *elf_symbol, Elf32_Shdr *sections)
+{
+	char			type = '?';
+	unsigned char	bind = ELF32_ST_BIND(elf_symbol->st_info);
+
+	if (bind == STB_WEAK && ELF32_ST_TYPE(elf_symbol->st_info) == STT_OBJECT)
+		type = elf_symbol->st_shndx == SHN_UNDEF ? 'v' : 'V';
+	else if (bind == STB_WEAK)
+		type = elf_symbol->st_shndx == SHN_UNDEF ? 'w' : 'W';
+	else if (elf_symbol->st_shndx == SHN_UNDEF)
+		type = bind == STB_LOCAL ? 'u' : 'U';
+	else if (elf_symbol->st_shndx == SHN_ABS)
+		type = bind == STB_LOCAL ? 'a' : 'A';
+	else if (elf_symbol->st_shndx == SHN_COMMON)
+		type = bind == STB_LOCAL ? 'c' : 'C';
+	else if (sections[elf_symbol->st_shndx].sh_type == SHT_NOBITS
+		&& sections[elf_symbol->st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE))
+		type = bind == STB_LOCAL ? 'b' : 'B';
+	else if (sections[elf_symbol->st_shndx].sh_type == SHT_DYNAMIC ||
+		((sections[elf_symbol->st_shndx].sh_type == SHT_PROGBITS
+		|| sections[elf_symbol->st_shndx].sh_type == SHT_INIT_ARRAY
+		|| sections[elf_symbol->st_shndx].sh_type == SHT_FINI_ARRAY)
+		&& sections[elf_symbol->st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE)))
+		type = bind == STB_LOCAL ? 'd' : 'D';
+	else if (sections[elf_symbol->st_shndx].sh_type == SHT_PREINIT_ARRAY)
+		type = 'D';
+	else if (sections[elf_symbol->st_shndx].sh_type == SHT_PROGBITS
+		&& sections[elf_symbol->st_shndx].sh_flags
+			== (SHF_ALLOC | SHF_EXECINSTR))
+		type = bind == STB_LOCAL ? 't' : 'T';
+	else if (ELF32_ST_TYPE(elf_symbol->st_info) == STT_SECTION
+		&& sections[elf_symbol->st_shndx].sh_type == SHT_PROGBITS
+		&& sections[elf_symbol->st_shndx].sh_flags == (SHF_MERGE | SHF_STRINGS))
+		type = bind == STB_LOCAL ? 'n' : 'N';
+	else if (!(sections[elf_symbol->st_shndx].sh_flags & SHF_WRITE))
+		type = bind == STB_LOCAL ? 'r' : 'R';
+	return (type);
+}
+
+static char	*symbol_name32(Elf32_Sym *elf_symbol, t_nm_file *file)
+{
+	if (ELF32_ST_TYPE(elf_symbol->st_info) == STT_SECTION)
+	{
+		if (elf_symbol->st_shndx >= file->sections_count)
+			return (NULL);
+		else if (file->sections.hdr32[elf_symbol->st_shndx].sh_name
+			>= file->shstrtab_size)
+			return (NULL);
+		return (file->shstrtab
+			+ file->sections.hdr32[elf_symbol->st_shndx].sh_name);
+	}
+	else
+	{
+		if (elf_symbol->st_name >= file->strtab_size)
+			return (NULL);
+		return (file->strtab + elf_symbol->st_name);
+	}
+}
+
+t_list	*push_symbol32(t_list **dest, Elf32_Sym *elf_symbol, t_nm_file *file)
+{
+	t_nm_symbol	symbol = { 0 };
+
+	if (elf_symbol->st_name >= file->strtab_size)
+		return (NULL);
+	if (!(symbol.name = symbol_name32(elf_symbol, file)))
+		return (NULL);
+	symbol.type = symbol_type32(elf_symbol, file->sections.hdr32);
+	symbol.value = elf_symbol->st_value;
+	return (ft_lst_push_back(dest, &symbol, sizeof(symbol)));
+}
+
 char	symbol_type64(Elf64_Sym *elf_symbol, Elf64_Shdr *sections)
 {
 	char			type = '?';
@@ -56,40 +128,6 @@ char	symbol_type64(Elf64_Sym *elf_symbol, Elf64_Shdr *sections)
 	else if (!(sections[elf_symbol->st_shndx].sh_flags & SHF_WRITE))
 		type = bind == STB_LOCAL ? 'r' : 'R';
 	return (type);
-}
-
-static char	*symbol_name32(Elf32_Sym *elf_symbol, t_nm_file *file)
-{
-	if (ELF32_ST_TYPE(elf_symbol->st_info) == STT_SECTION)
-	{
-		if (elf_symbol->st_shndx >= file->sections_count)
-			return (NULL);
-		else if (file->sections.hdr32[elf_symbol->st_shndx].sh_name
-			>= file->shstrtab_size)
-			return (NULL);
-		return (file->shstrtab
-			+ file->sections.hdr32[elf_symbol->st_shndx].sh_name);
-	}
-	else
-	{
-		if (elf_symbol->st_name >= file->strtab_size)
-			return (NULL);
-		return (file->strtab + elf_symbol->st_name);
-	}
-}
-
-t_list	*push_symbol32(t_list **dest, Elf32_Sym *elf_symbol, t_nm_file *file)
-{
-	t_nm_symbol	symbol = { 0 };
-
-	if (elf_symbol->st_name >= file->strtab_size)
-		return (NULL);
-	if (!(symbol.name = symbol_name32(elf_symbol, file)))
-		return (NULL);
-	//symbol.type = symbol_type32(elf_symbol, file->sections.hdr32);
-	symbol.type = 'U'; //TEMP
-	symbol.value = elf_symbol->st_value;
-	return (ft_lst_push_back(dest, &symbol, sizeof(symbol)));
 }
 
 static char	*symbol_name64(Elf64_Sym *elf_symbol, t_nm_file *file)
