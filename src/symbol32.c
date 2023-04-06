adc4ab7c75b9e45b949b973cc6f552fc2f46fb90
Yoann FOREAU <yoyo.fofo47@gmail.com>
2023-04-06 19:41:01 +0200

    refactor: split symbol type function in two

--- src/symbol32.c
+++ src/symbol32.c
@@ -12,49 +12,54 @@
 
 #include "ft_nm.h"
 
+static char	symbol_type_no_section32(Elf32_Sym *elf_symbol)
+{
+	unsigned char	st_bind = ELF32_ST_BIND(elf_symbol->st_info);
+	unsigned char	st_type = ELF32_ST_TYPE(elf_symbol->st_info);
+	uint32_t		st_shndx = elf_symbol->st_shndx;
+
+	if (st_bind == STB_WEAK && st_type == STT_OBJECT)
+		return (st_shndx == SHN_UNDEF ? 'v' : 'V');
+	else if (st_bind == STB_WEAK)
+		return (st_shndx == SHN_UNDEF ? 'w' : 'W');
+	else if (st_shndx == SHN_UNDEF)
+		return (st_bind == STB_LOCAL ? 'u' : 'U');
+	else if (st_shndx == SHN_ABS)
+		return (st_bind == STB_LOCAL ? 'a' : 'A');
+	else if (st_shndx == SHN_COMMON)
+		return (st_bind == STB_LOCAL ? 'c' : 'C');
+	return '?';
+}
+
 static char	symbol_type32(Elf32_Sym *elf_symbol, Elf32_Shdr *sections)
 {
-	char			type = '?';
-	unsigned char	bind = ELF32_ST_BIND(elf_symbol->st_info);
+	char			type;
+	unsigned char	st_bind = ELF32_ST_BIND(elf_symbol->st_info);
+	unsigned char	st_type = ELF32_ST_TYPE(elf_symbol->st_info);
+	uint32_t		sh_type, sh_flags;
 
-	if (bind == STB_WEAK && ELF32_ST_TYPE(elf_symbol->st_info) == STT_OBJECT)
-		type = elf_symbol->st_shndx == SHN_UNDEF ? 'v' : 'V';
-	else if (bind == STB_WEAK)
-		type = elf_symbol->st_shndx == SHN_UNDEF ? 'w' : 'W';
-	else if (elf_symbol->st_shndx == SHN_UNDEF)
-		type = bind == STB_LOCAL ? 'u' : 'U';
-	else if (elf_symbol->st_shndx == SHN_ABS)
-		type = bind == STB_LOCAL ? 'a' : 'A';
-	else if (elf_symbol->st_shndx == SHN_COMMON)
-		type = bind == STB_LOCAL ? 'c' : 'C';
-	else if (sections[elf_symbol->st_shndx].sh_type == SHT_NOBITS
-		&& (sections[elf_symbol->st_shndx].sh_flags & (SHF_ALLOC | SHF_WRITE)))
-		type = bind == STB_LOCAL ? 'b' : 'B';
-	else if (sections[elf_symbol->st_shndx].sh_type == SHT_DYNAMIC ||
-		((sections[elf_symbol->st_shndx].sh_type == SHT_PROGBITS
-		|| sections[elf_symbol->st_shndx].sh_type == SHT_INIT_ARRAY
-		|| sections[elf_symbol->st_shndx].sh_type == SHT_FINI_ARRAY)
-		&& (sections[elf_symbol->st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE)
-		|| sections[elf_symbol->st_shndx].sh_flags == 1027)))
-		type = bind == STB_LOCAL ? 'd' : 'D';
-	else if (sections[elf_symbol->st_shndx].sh_type == SHT_PREINIT_ARRAY)
+	if ((type = symbol_type_no_section32(elf_symbol)) != '?')
+		return (type);
+	sh_type = sections[elf_symbol->st_shndx].sh_type;
+	sh_flags = sections[elf_symbol->st_shndx].sh_flags;
+	if (sh_type == SHT_NOBITS && (sh_flags & (SHF_ALLOC | SHF_WRITE)))
+		type = st_bind == STB_LOCAL ? 'b' : 'B';
+	else if (sh_type == SHT_DYNAMIC || ((sh_type == SHT_PROGBITS
+		|| sh_type == SHT_INIT_ARRAY || sh_type == SHT_FINI_ARRAY)
+		&& (sh_flags == (SHF_ALLOC | SHF_WRITE) || sh_flags == 1027)))
+		type = st_bind == STB_LOCAL ? 'd' : 'D';
+	else if (sh_type == SHT_PREINIT_ARRAY)
 		type = 'D';
-	else if (sections[elf_symbol->st_shndx].sh_type == SHT_PROGBITS
-		&& sections[elf_symbol->st_shndx].sh_flags
-			== (SHF_ALLOC | SHF_EXECINSTR))
-		type = bind == STB_LOCAL ? 't' : 'T';
-	else if (ELF32_ST_TYPE(elf_symbol->st_info) == STT_SECTION
-		&& sections[elf_symbol->st_shndx].sh_type == SHT_PROGBITS
-		&& sections[elf_symbol->st_shndx].sh_flags == (SHF_MERGE | SHF_STRINGS))
-		type = bind == STB_LOCAL ? 'n' : 'N';
-	else if (ELF32_ST_TYPE(elf_symbol->st_info) == STT_SECTION
-		&& sections[elf_symbol->st_shndx].sh_type == SHT_PROGBITS
-		&& (!sections[elf_symbol->st_shndx].sh_flags
-		|| sections[elf_symbol->st_shndx].sh_flags == 2048
-		|| sections[elf_symbol->st_shndx].sh_flags == 2096))
+	else if (sh_type == SHT_PROGBITS && sh_flags == (SHF_ALLOC | SHF_EXECINSTR))
+		type = st_bind == STB_LOCAL ? 't' : 'T';
+	else if (st_type == STT_SECTION && sh_type == SHT_PROGBITS
+		&& sh_flags == (SHF_MERGE | SHF_STRINGS))
+		type = st_bind == STB_LOCAL ? 'n' : 'N';
+	else if (st_type == STT_SECTION && sh_type == SHT_PROGBITS && (!sh_flags
+		|| sh_flags == 2048 || sh_flags == 2096))
 		type = 'N';
-	else if (!(sections[elf_symbol->st_shndx].sh_flags & SHF_WRITE))
-		type = bind == STB_LOCAL ? 'r' : 'R';
+	else if (!(sh_flags & SHF_WRITE))
+		type = st_bind == STB_LOCAL ? 'r' : 'R';
 	return (type);
 }
 
