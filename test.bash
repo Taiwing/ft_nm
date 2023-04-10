7889c095902dc971bf5a85fe69278b021b005a65
Yoann FOREAU <yforeau@e2r8p15.clusters.42paris.fr>
2023-04-10 19:26:19 +0200

    chore: cleanup directory

--- test.bash
+++ test.bash
@@ -7,7 +7,7 @@ OPTS=""
 for OPTS in "" "-a" "-g" "-u" "-ag" "-au" "-gu" "-agu" "-agurp"; do
 	echo
 	echo "Testing with options: $OPTS"
-	for file in $(ls bin/*); do
+	for file in $(ls resources/bin/*); do
 	    echo "Testing $file"
 	    $MY_NM $OPTS $file > my.out
 	    $OG_NM $OPTS $file > og.out
